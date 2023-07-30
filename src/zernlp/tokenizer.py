6bf12522162afc622f77758c14e5f143aa5fb39f
kennethtegrado <kategrado@up.edu.ph>
2023-07-30 19:20:49 +0800

    add init tokenizer

---
+++ src/zernlp/tokenizer.py
@@ -0,0 +1,8 @@
+class SentenceTokenizer:
+    def __init__():
+        print("Successfully added sentence tokenizer")
+        pass
+
+class WordTokenizer:
+    def __init__():
+        print("Successfully added word tokenizer")
